918935a962a8125fd16c86bc1fef1866677bc5d4
ChrisLiutx <63918785+ChrisLiutx@users.noreply.github.com>
2022-01-17 22:40:16 -0800

    Kinda good, for benchmarking

--- kMC.py
+++ kMC.py
@@ -2,6 +2,26 @@
 from tqdm import tqdm #progress bar
 import matplotlib.pyplot as plt
 import numpy as np
+import queue as q
+import timeit
+
+"""
+type of node: 0: top interface, 1: bottom interface, 2: bulk
+0: "generation"
+1: "annihilation"
+2: "diffusion"
+"""
+
+k_values = [1,0,0,1,0,0,1,0,0]
+
+def convert(seconds):
+    seconds = seconds % (24 * 3600)
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
 
 class Simulation():
     """
@@ -11,103 +31,196 @@ class Simulation():
         self.length = length
         self.width = width
         self.height = height
-        self.grid = self.generate_nodes() #grid[h][w][l]
+        self.grid = self.generate_nodes() #grid[l][w][h]
         self.clock = 0
         self.cycle = 0
+        self.k_total = 0
 
     def generate_nodes(self):
         """
         Generates nodes of the simulation
         """
-        return [[[Node(l,w,h) for h in range(self.height)] for w in range(self.width)] for l in range(self.length)]
+        return [[[Node(l,w,h,depth=self.height) for h in range(self.height)] for w in range(self.width)] for l in range(self.length)]
 
     def generate_process(self):
         """
         Generates the processes available currently. Run every cycle of simulation
         """
-        processes = []
-        k_total = 0
         for layers in self.grid:
             for layer in layers:
                 for node in layer:
                     for p in node.generate_process():
-                        processes.append(p)
-                        k_total += p.kp
-        return processes, k_total
+                        self.k_total += p.kp
 
     def next_cycle(self):
         """
         Runs the simulation for 1 cycle
         """
         self.cycle += 1 #increment cycle count
-        processes, k_total = self.generate_process() #Determine all possible processes p for current state and k_total
         ran1, ran2 = np.random.uniform(low=0.0, high=1.0, size=2) #Draw 2 random numbers from uniform [0,1]
         #Extract process q such that Sum(p=1, q)kp >= ran1 * k_total >= Sum(p=1, q-1) kp
-        goal = ran1 * k_total
+        goal = ran1 * self.k_total
         sum_kp = 0
         selected = None
-        for process in processes:
-            sum_kp += process.kp
-            if sum_kp >= goal:
-                selected = process
+        flag = False
+        for layers in self.grid:
+            for layer in layers:
+                for node in layer:
+                    for p in node.processes:
+                        sum_kp += p.kp
+                        if sum_kp >= goal:
+                            selected = p
+                            flag = True
+                            break
+                    if flag:
+                        break
+                if flag:
+                    break
+            if flag:
                 break
-        selected.execute() #Execute selected process
-        self.clock -= (np.log(ran2)/k_total)
+        self.k_total += selected.execute() #Execute selected process
+        self.clock -= (np.log(ran2)/self.k_total)
+        print(self.cycle)
     
     def run(self):
         """
         Runs the simulation till completion
         """
-        while bfs(): #FIXME: replace condition for ending. Eg: BFS search found path
+        start = timeit.default_timer()
+        visual = Display()
+        print("\n")
+        self.generate_process()
+        print(self.k_total)
+        while not (finalnode := bfs(self.grid)): #FIXME: replace condition for ending. Eg: BFS search found path
             self.next_cycle()
-        print(f"Simulation completed. Cycles: {self.cycle}, Clock: {self.clock}")
+            # visual.voxel_visualize(self.grid)
+        stop = timeit.default_timer()
+        sim_time = convert(stop-start)
+        print(f"Simulation took {sim_time}. Cycles: {self.cycle}, Clock: {self.clock}")
+        visual.voxel_visualize(self.grid)
+        visual.show_path(self.grid, finalnode)
+        wait = input("Done.")
 
 
 class Node():
     """
     Node Class
     """
-    def __init__(self, length, width, height):
+    def __init__(self, length, width, height, depth=0):
         self.length = length
         self.width = width
         self.height = height
         self.processes = []
+        if height == 0:
+            self.nodetype = 0
+        elif height == depth-1:
+            self.nodetype = 1
+        else:
+            self.nodetype = 2
+        self.defect = False
+        self.num_defect_neighbors = 0
     
     def generate_process(self):
         self.processes = []
-        if True:
-            self.processes.append(Process(node=self, kp=5))
+        if not self.defect:
+            self.processes.append(Process(node=self, ptype=0))
+        else:
+            self.processes.append(Process(node=self, ptype=1))
+            for i in range(self.num_defect_neighbors):
+                self.processes.append(Process(node=self, ptype=2))
         return self.processes
 
 class Process(Node):
     """
     Process Class
     """
-    def __init__(self, node=None, kp=0): #FIXME Figure out how kp is determined
+    def __init__(self, node=None, ptype=None, alpha=1, to_node=None): #FIXME Figure out how kp is determined
         self.node = node
         #kp to be determined
-        self.kp = kp
+        self.ptype = ptype
+        self.kp = k_values[self.node.nodetype * 3 + self.ptype]
 
     def execute(self): #FIXME Remember to do this
-        pass
+        if self.ptype == 0:
+            self.node.defect = True
+            self.node.processes = []
+            # self.node.processes.remove(self)
+            return -self.kp
+        elif self.ptype == 1:
+            self.node.defect = False
+        else:
+            pass #FIXME
 
+class Display():
+    def __init__(self):
+        self.ax = plt.figure().add_subplot(projection="3d")
 
-def bfs():
+    def voxel_visualize(self, tgrid):
+        """
+        Voxel visualization, might consider using grid view later
+        """
+        self.grid = [[[node.defect for node in layer]for layer in layers] for layers in tgrid]
+        self.ax.clear()
+        self.ax.voxels(np.array(self.grid), facecolors='#1f77b430', edgecolor='k', shade=False)
+        plt.pause(1)
+    
+    def show_path(self, tgrid, finalnode):
+        pathgrid = [[[False for node in layer]for layer in layers] for layers in tgrid]
+        while finalnode != None:
+            pos = finalnode.pos
+            pathgrid[pos[0]][pos[1]][pos[2]] = True
+            finalnode = finalnode.parent
+        self.ax.voxels(np.array(pathgrid), facecolors='#FF000080', edgecolor='k', shade=False)
+        plt.show()
+
+class SearchNode():
+    def __init__(self, pos, parent=None):
+        self.pos = pos
+        self.parent = parent
+
+pos_diag = [[0,1,1], [0,-1,1], [1,0,1], [-1,0,1], [1,1,1], [-1,-1,1], [1,-1,1], [-1,1,1], [0,0,1], [0,1,0], [0,-1,0], [1,0,0], [-1,0,0], [1,1,0], [-1,-1,0], [1,-1,0], [-1,1,0], [0,1,-1], [0,-1,-1], [1,0,-1], [-1,0,-1], [1,1,-1], [-1,-1,-1], [1,-1,-1], [-1,1,-1], [0,0,-1]]
+
+pos_no_diag = [[0,0,1], [0,1,0], [0,-1,0], [1,0,0], [-1,0,0], [0,0,-1]]
+
+def bfs(tgrid, diagonal=False):
     """
     Breadth First Search Algo
     """
-    pass
+    grid = tgrid
+    queue = q.Queue()
+    visited = set()
+    length = len(grid)
+    width = len(grid[0])
+    height = len(grid[0][0])
+    for x in range(length):
+        for y in range(width):
+            temp = np.array([x,y,height-1])
+            if grid[temp[0]][temp[1]][temp[2]].defect:
+                queue.put(SearchNode(temp))
+                visited.add(tuple(temp))
+    while not queue.empty():
+        currNode = queue.get()
+        curr = currNode.pos
+        if diagonal:
+            pos = pos_diag
+        else:
+            pos = pos_no_diag
+        for p in pos:
+            temp = curr + np.array(p)
+            if tuple(temp) in visited or any(x < 0 for x in temp) or temp[0] > length-1 or temp[1] > width-1 or temp[2] > height-1:
+                continue
+            if grid[temp[0]][temp[1]][temp[2]].defect:
+                if temp[2] == 0:
+                    print("Found")
+                    return SearchNode(temp, currNode)
+                queue.put(SearchNode(temp, currNode))
+                visited.add(tuple(temp))
 
-def voxel_visualize(grid):
-    """
-    Voxel visualization, might consider using grid view later
-    """
-    ax = plt.figure().add_subplot(projection="3d")
-    ax.voxels(np.array(grid), facecolors='#1f77b430', edgecolor='k', shade=False)
-    plt.show()
+    return False
 
 if __name__ == "__main__":
     for i in tqdm(range(1)): #Number of times to run simulation
-        pass
+        sim = Simulation(350,350,5)
+        sim.run()
 else:
     print("Please read documentation")
