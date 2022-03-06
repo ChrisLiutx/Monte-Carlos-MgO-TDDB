237de82f960b2d2aa413b43df2b67eb0adf152c0
ChrisLiutx <63918785+ChrisLiutx@users.noreply.github.com>
2022-03-05 21:34:56 -0800

    Completed outputting

--- run.py
+++ run.py
@@ -13,12 +13,13 @@ if __name__ == "__main__":
     start = timeit.default_timer()
 
     ############# PARAMETERS ###############
-    runs = 1 # Number of runs
-    interface_multipliers = [1] # List of interface multiplier values
+    runs = 100 # Number of runs
+    interface_multipliers = [1, 5, 10, 15, 20] # List of interface multiplier values
     heights = [5] # List of height values. Use range() if you want a range of values
     k = 2.718281828 # Default k value
     demo = 0 # Set demo to 1 to turn on display
     demo_delay = 0.5 #Time delay between cycles for Demo
+    directory = "/output/" #Folder for output, start and end with '/'
     ############# PARAMETERS ###############
 
     total_num_simulations = runs*len(interface_multipliers)*len(heights)
@@ -37,9 +38,11 @@ if __name__ == "__main__":
                         "bulkDiffusion": 0,
                         "bulkAnnihilation": 0
                     }
-                    sim = Simulation(5, 5, 5, k_values, interface_multiplier)
+                    sim = Simulation(50, 50, 5, k_values, interface_multiplier)
                     sim.run(demo=demo, demo_delay=demo_delay)
-                    test = input("End: ") #TODO REMOVE THIS
+                    sim.generate_output(directory=directory)
+                    if demo:
+                        test = input("End: ") #TODO REMOVE THIS
                     pbar.update(1)
 
     #Prints total runtime
