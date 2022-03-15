3b59a07b1c313fc1a298fe267b2bd2c7bccce5fb
Sparks343 <46782359+Sparks343@users.noreply.github.com>
2022-03-15 11:15:06 +0800

    Plotting and Organizing
    Plotted results in 50x50x5
    Organized the outputs

--- run_joel.py
+++ run_joel.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     interface_multipliers = [1] # List of interface multiplier values
     heights = [5] # List of height values. Use range() if you want a range of values
     ks = np.arange(0.76,0.95,0.02)
-    ks = 1/(236*0.0002*np.power(ks,-27.15))
+    ks = 1/(235*0.0002*np.power(ks,-27.15))
     # k = 1/(50*0.0002*0.90**-27.15) # Default k value
     demo = 0 # Set demo to 1 to turn on display
     demo_delay = 0.5 #Time delay between cycles for Demo
